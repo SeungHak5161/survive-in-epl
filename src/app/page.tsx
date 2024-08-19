f993ee7bd512d59691e423442b414625d434b548
SeungHak <bsh93367@gmail.com>
2024-08-20 01:57:49 +0900

    feat: 메인페이지 예측 순위 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import Matches from "@/components/Matches/Matches"
+import PredictSimple from "@/components/Predict/PredictSimple"
 import Standings from "@/components/Standings/Standings"
 import useMatchess from "@/hooks/useMatches"
+import usePredict from "@/hooks/usePredict"
 import useStandings from "@/hooks/useStandings"
 
 export default function Home() {
   const { data: standings, isLoading: isStandingsLoading } = useStandings()
   const { data: matches, isLoading: isMatchesLoading } = useMatchess()
+  const { data: predictData, isLoading: isPredictDataLoading } = usePredict()
 
   return (
     <div className="mx-auto max-w-[1200px] px-3 py-5 max-md:px-5">
@@ -15,11 +18,13 @@ export default function Home() {
         <div className="col-span-2">
           <Matches matches={matches} isLoading={isMatchesLoading} />
         </div>
-        <div className="">예측 순위</div>
+        <div className="">
+          <PredictSimple predictData={predictData} isLoading={isPredictDataLoading} />
+        </div>
         <div className="row-span-2">
           <Standings standings={standings} isLoading={isStandingsLoading} />
         </div>
-        <div className="">선수 스탯</div>
+        {/* <div className="">선수 스탯</div> */}
       </div>
     </div>
   )
