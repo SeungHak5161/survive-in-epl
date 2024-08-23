823356ac1728719c5dc96086d46c7820f4eb3bad
SeungHak <bsh93367@gmail.com>
2024-08-23 19:12:54 +0900

    update: 메인 페이지 예측 순위 5명만 보이게

--- src/components/Predict/PredictSimple.tsx
+++ src/components/Predict/PredictSimple.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useEffect, useState } from "react"
+
 import Spinner from "../Spinner/Spinner"
 
 const PredictSimple = ({
@@ -9,6 +11,12 @@ const PredictSimple = ({
   predictData: IPredict[] | undefined
   isLoading: boolean
 }) => {
+  const [predictList, setPredictList] = useState<IPredict[] | undefined>(undefined)
+  useEffect(() => {
+    if (isLoading || !predictData) return
+    const newPredictList = predictData.slice(0, 5)
+    setPredictList(newPredictList)
+  }, [predictData])
   return (
     <div className="rounded-xl bg-white p-3 pb-1">
       {isLoading || !predictData ? (
@@ -22,24 +30,25 @@ const PredictSimple = ({
             <div className="col-span-2">이름</div>
             <div>유사도</div>
           </div>
-          {predictData.map((obj, idx) => {
-            return (
-              <div
-                key={idx}
-                className={`grid grid-cols-4 items-center py-2 ${idx === predictData.length - 1 || "border-b-[1px] border-b-slate-200"}`}
-              >
-                <div className="flex items-center justify-center">
-                  <div
-                    className={`flex w-8 items-center justify-center rounded-md ${idx + 1 === 1 ? "bg-gradient-to-br from-blue-600 to-orange-500" : "bg-epl_purple"} py-1 font-bold text-white`}
-                  >
-                    {idx + 1}
+          {predictList &&
+            predictList.map((obj, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className={`grid grid-cols-4 items-center py-2 ${idx === predictList.length - 1 || "border-b-[1px] border-b-slate-200"}`}
+                >
+                  <div className="flex items-center justify-center">
+                    <div
+                      className={`flex w-8 items-center justify-center rounded-md ${idx + 1 === 1 ? "bg-gradient-to-br from-blue-600 to-orange-500" : "bg-epl_purple"} py-1 font-bold text-white`}
+                    >
+                      {idx + 1}
+                    </div>
                   </div>
+                  <span className="col-span-2 font-semibold">{obj.name}</span>
+                  <span className="flex justify-center font-bold">{obj.accuracy.toFixed(1)}%</span>
                 </div>
-                <span className="col-span-2 font-semibold">{obj.name}</span>
-                <span className="flex justify-center font-bold">{obj.accuracy.toFixed(1)}%</span>
-              </div>
-            )
-          })}
+              )
+            })}
         </div>
       )}
     </div>
